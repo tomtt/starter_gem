405907222f175c87535508d58eabfa3330596f57
Tom ten Thij <tom.tenthij@fuga.com>
2021-03-30 19:25:57 +0200

    wip: some test curses output

--- lib/starter_gem/do_something.rb
+++ lib/starter_gem/do_something.rb
@@ -1,4 +1,5 @@
 require "pastel"
+require "curses"
 
 Encoding.default_external = "UTF-8"
 
@@ -17,7 +18,23 @@ module StarterGem
                     else
                       "? #{@options[:color]}"
                     end
-      puts output_text
+      Curses.init_screen
+
+      begin
+        Curses.start_color
+        Curses.noecho
+        # Curses.cbreak
+        Curses.init_pair(1, Curses::COLOR_BLACK, Curses::COLOR_YELLOW)
+        Curses.attrset(Curses.color_pair(1) | Curses::A_BLINK)
+        10.times do |i|
+          Curses.setpos(i,5*i)
+          Curses.addstr("Hello #{i+1}/10")
+          Curses.getch
+        end
+      ensure
+        Curses.close_screen
+        puts output_text
+      end
     end
   end
 end
